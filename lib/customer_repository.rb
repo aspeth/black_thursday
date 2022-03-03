d1d8fcfebe1efda8e835417b3eab57c93a07f8d8
Sherman Au <shermau13@gmail.com>
2022-03-03 01:08:39 -0700

    refactor:repo_module to customer_repo

--- lib/customer_repository.rb
+++ lib/customer_repository.rb
@@ -1,40 +1,42 @@
 # frozen_string_literal: true
 
+require_relative 'repo_module'
 class CustomerRepository
-  attr_reader :customers
+  include RepoModule
+  attr_reader :repo
 
   def initialize(file)
-    @customers = []
+    @repo = []
     open_customers(file)
   end
 
   def open_customers(file)
     CSV.foreach(file, headers: true, header_converters: :symbol) do |row|
-      @customers << Customer.new(row)
+      @repo << Customer.new(row)
     end
   end
 
-  def all
-    @customers
-  end
+  # def all
+  #  @repo
+  # end
 
   def find_by_id(id)
-    @customers.find { |customer| customer.id == id }
+    @repo.find { |customer| customer.id == id }
   end
 
   def find_all_by_first_name(name)
-    @customers.find_all { |customer| customer.first_name.downcase.include?(name.downcase) }
+    @repo.find_all { |customer| customer.first_name.downcase.include?(name.downcase) }
   end
 
   def find_all_by_last_name(name)
-    @customers.find_all { |customer| customer.last_name.downcase.include?(name.downcase) }
+    @repo.find_all { |customer| customer.last_name.downcase.include?(name.downcase) }
   end
 
   def create(attributes)
-    @customers.sort_by(&:id)
-    last_id = @customers.last.id
+    @repo.sort_by(&:id)
+    last_id = @repo.last.id
     attributes[:id] = (last_id += 1)
-    @customers << Customer.new(attributes)
+    @repo << Customer.new(attributes)
   end
 
   def update(id, attributes)
@@ -47,7 +49,7 @@ class CustomerRepository
   end
 
   def delete(id)
-    @customers.delete(find_by_id(id))
+    @repo.delete(find_by_id(id))
   end
 
   def inspect
